ce02345ce307c567be9ab89723feb882dec884bd
Abdul <abdulkaleem8360@gmail.com>
2023-07-09 12:51:39 +0530

    Added 153 leetcode problrm

---
+++ FindMinimum(153).java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findMin(int[] nums) {
+       int s=0;
+       int e=nums.length-1;
+       while(s<e){
+           int mid=s+(e-s)/2;
+           if(nums[mid]>nums[e]){
+               s=mid+1;
+           }
+           else{
+               e=mid;
+                          }
+       }
+       return nums[s];
+
+    }
+}
