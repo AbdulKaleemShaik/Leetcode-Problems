24a2267ff8c4de54c215e26b9565473e496ee40f
Abdul <abdulkaleem8360@gmail.com>
2023-07-21 10:52:20 +0530

    Added 14 leetcode problem

---
+++ longestCommonPre(14).java
@@ -0,0 +1,8 @@
+class Solution {
+    public String longestCommonPrefix(String[] arr) {
+        if(arr.length==0)return "";
+        String op=arr[0];
+        for(int i=1; i<arr.length; i++)while(arr[i].indexOf(op)!=0)op=op.substring(0, op.length()-1);
+        return op;
+    }
+}
