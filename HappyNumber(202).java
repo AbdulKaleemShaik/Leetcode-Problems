fdac819880c4cf9296517a2c6500a012d9a86b7d
Abdul <abdulkaleem8360@gmail.com>
2023-08-02 16:49:52 +0530

    Added 202 leetcode problem

---
+++ HappyNumber(202).java
@@ -0,0 +1,27 @@
+class Solution {
+    public static  boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+        do {
+            slow = find(slow);
+            fast = find(find(fast));
+        } while (slow != fast);
+        if (slow == 1) {
+            return true;
+        }
+        return false;
+    }
+
+    private static int find(int num) {
+        int ans = 0;
+        while (num > 0) {
+            int rem = num % 10;
+            ans += rem * rem;
+            num /= 10;
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        System.out.println(isHappy(19));
+    }
+}
