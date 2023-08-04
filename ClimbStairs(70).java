6e6f23e483c85d37893f420e3d305c7469005219
Abdul <abdulkaleem8360@gmail.com>
2023-08-04 21:03:21 +0530

    Added 70 leetcode problem

---
+++ ClimbStairs(70).java
@@ -0,0 +1,14 @@
+class Solution {
+    public static  int climbStairs(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        int a = 1, b = 1;
+        for (int i = 2; i <= n; i++) {
+            int c =a+b;
+            a = b;
+            b=c;
+        }
+        return b;
+    }
+}
