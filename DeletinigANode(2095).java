54ce0f7f53669dcc8b03aaab25ac5d2793d0c6fb
Abdul <abdulkaleem8360@gmail.com>
2023-08-29 15:23:12 +0530

    Added 2095 leetcode problem

---
+++ DeletinigANode(2095).java
@@ -0,0 +1,36 @@
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if (head == null || head.next == null) {
+            return head.next;
+        } else {
+            prev.next = slow.next;
+        }
+        return head;
+    }
+}
