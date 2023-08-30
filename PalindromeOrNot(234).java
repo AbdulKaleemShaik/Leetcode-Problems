8bdf17b36e2a3db6e1724b1f82f01cd5acf440fb
Abdul <abdulkaleem8360@gmail.com>
2023-08-30 08:36:43 +0530

    Added 234 leetcode problem

---
+++ PalindromeOrNot(234).java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode temp=head;
+        int num=0;
+        int rev=0;
+        int mul=1;
+        while(temp!=null){
+            num=num*10+temp.val;
+            rev=rev+temp.val*mul;
+            mul=mul*10;
+            temp=temp.next;
+        }
+        return num==rev;
+    }
+}
