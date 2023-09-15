5a8e245be18c8c474d836a8eaf99170e001e16f4
Abdul <abdulkaleem8360@gmail.com>
2023-09-15 11:00:12 +0530

    Added 107 leetcode problem

---
+++ BinaryLevelOrdertraversal(107).java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> qr = new LinkedList<>();
+        qr.add(root);
+        while (!qr.isEmpty()) {
+            int size = qr.size();
+            List<Integer> curlevel = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode curnode = qr.poll();
+                curlevel.add(curnode.val);
+                if (curnode.left != null) {
+                    qr.add(curnode.left);
+                }
+                if (curnode.right != null) {
+                    qr.add(curnode.right);
+                }
+            }
+            result.add(0,curlevel);
+
+        }
+
+        return result;
+    }
+}
