d2a960d86cc43bd1ba6542d504cddea6a4951fcb
Abdul <abdulkaleem8360@gmail.com>
2024-02-08 20:31:20 +0530

    Added 2974 leetcode problem

---
+++ MinimumNumberGame(2974).java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] numberGame(int[] nums) {
+        int[] arr = new int[nums.length];
+        Arrays.sort(nums);
+        // int k=0,j=1,h=0;
+        // for(int i=0;i<(nums.length)/2;i++){
+        // arr[h]=nums[j];
+        // h++;
+        // arr[h]=nums[k];
+        // h++;
+        // j+=2;
+        // k+=2;
+        // }
+        for (int i = 0; i < nums.length - 1; i += 2) {
+            int a = nums[i];
+            nums[i] = nums[i + 1];
+            nums[i + 1] = a;
+        }
+        return nums;
+
+    }
+}
