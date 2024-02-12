a9b07e2982b9425da66018d0eafc8edccfb472fa
Abdul <abdulkaleem8360@gmail.com>
2024-02-12 16:16:32 +0530

    Added 148th leetcode problem

---
+++ SortList(148).java
@@ -0,0 +1,46 @@
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode prev = null;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null;
+        ListNode left = sortList(head);
+        ListNode right = sortList(slow);
+
+        return merge(left, right);
+    }
+
+    public ListNode merge(ListNode left, ListNode right) {
+        ListNode ans = new ListNode(0);
+        ListNode temp = ans;
+        while (left != null && right != null) {
+            if (left.val <= right.val) {
+                temp.next = left;
+                left = left.next;
+            } else {
+                temp.next = right;
+                right = right.next;
+            }
+            temp = temp.next;
+        }
+        while (left != null) {
+            temp.next = left;
+            left = left.next;
+            temp = temp.next;
+        }
+        while (right != null) {
+            temp.next = right;
+            right = right.next;
+            temp = temp.next;
+        }
+        return ans.next;
+    }
+}
