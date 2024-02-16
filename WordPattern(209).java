988af906fe23e48c94a6dededa8a1e35797d3030
Abdul <abdulkaleem8360@gmail.com>
2024-02-16 21:12:03 +0530

    Added 209th leetcode problem

---
+++ WordPattern(209).java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] word = s.split(" ");
+        if (word.length != pattern.length())
+            return false;
+
+        HashMap<Character, String> map = new HashMap();
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+
+            if (map.containsKey(c)) {
+                if (!map.get(c).equals(word[i])) {
+                    return false;
+                }
+            } else {
+                if (map.containsValue(word[i])) {
+                    return false;
+                }
+                map.put(c, word[i]);
+            }
+        }
+
+        return true;
+    }
+}
