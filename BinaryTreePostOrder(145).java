0633a4b41c1cbcedb170308b355b55df2a937ddd
Abdul <abdulkaleem8360@gmail.com>
2024-02-17 09:17:55 +0530

    Added 145th leetcode problem

---
+++ BinaryTreePostOrder(145).java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    List<Integer> ans = new ArrayList<>();
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        if (root == null) {
+            return ans;
+        }
+        postorderTraversal(root.left);
+        postorderTraversal(root.right);
+        ans.add(root.val);
+        return ans;
+    }
+}
