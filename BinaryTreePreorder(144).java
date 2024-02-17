a6188540b9ef5636823001a35b1c08254259166e
Abdul <abdulkaleem8360@gmail.com>
2024-02-17 09:10:49 +0530

    Added 144th leetcode problem

---
+++ BinaryTreePreorder(144).java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+ 
+class Solution {
+    List<Integer> ans=new ArrayList<>();
+  public List<Integer> preorderTraversal(TreeNode root) {
+      if(root==null){
+          return ans;
+      }
+      ans.add(root.val);
+      preorderTraversal(root.left);
+      preorderTraversal(root.right);
+      return ans;
+  }
+}
