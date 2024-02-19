df4bcf751ebb1bd1297012e5b20af2c8b2d1dfd5
Abdul <abdulkaleem8360@gmail.com>
2024-02-19 10:55:19 +0530

    Added 496th leetcode problem

---
+++ NextGreater(496).java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+        for (int num : nums2) {
+            while (!stack.empty() && stack.peek() < num) {
+                map.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+        for (int i = 0; i < nums1.length; i++) {
+            nums1[i] = map.containsKey(nums1[i]) ? map.get(nums1[i]) : -1;
+        }
+        return nums1;
+    }
+}
