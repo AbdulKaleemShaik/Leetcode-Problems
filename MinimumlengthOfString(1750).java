26e395564947b5896afe4f702098a66c1f4d16e9
Abdul <abdulkaleem8360@gmail.com>
2024-03-05 22:07:27 +0530

    Added 1750th leetcode problem

---
+++ MinimumlengthOfString(1750).java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minimumLength(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                break;
+            }
+            char c = s.charAt(left);
+            while (left < right && c == s.charAt(left)) {
+                left++;
+            }
+            while (left <= right && c == s.charAt(right)) {
+                right--;
+            }
+        }
+        return right - left + 1;
+    }
+}
