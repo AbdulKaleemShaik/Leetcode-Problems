1e1d4e173056e70b73fbaa54e80cd4e3ea1cafad
Abdul <abdulkaleem8360@gmail.com>
2024-03-06 13:27:12 +0530

    Added 542th leetcode problem

---
+++ 01Matrix(542).java
@@ -0,0 +1,52 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Pair{
+    int row;
+    int col;
+    int dist;
+    Pair(int row, int col, int dist){
+        this.row = row;
+        this.col = col;
+        this.dist = dist;
+    }
+}
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+        boolean[][] vis = new boolean[n][m];
+        int[][] ans = new int[n][m];
+
+        Queue<Pair> q = new LinkedList<>();
+
+        for(int i = 0; i < n; ++i){
+            for(int j = 0; j < m; ++j){
+                if(mat[i][j] == 0){
+                    q.add(new Pair(i, j , 0));
+                    vis[i][j] = true;
+                }
+            }
+        }
+        int[] drow = {-1, 0, 1, 0};
+        int[] dcol = {0, 1, 0, -1};
+
+        while(!q.isEmpty()){
+            int row = q.peek().row;
+            int col = q.peek().col;
+            int dist = q.peek().dist;
+            ans[row][col] = dist;
+            
+            q.poll();
+        for(int i = 0; i < 4; ++i){
+            int nrow = row + drow[i];
+            int ncol = col + dcol[i];
+            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] == false){
+                vis[nrow][ncol] = true;
+                q.add(new Pair(nrow, ncol, dist + 1));
+            }
+        }
+    }
+    return ans;
+    }
+}
