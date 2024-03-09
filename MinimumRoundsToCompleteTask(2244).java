0bd3e330b1d22ff7ebffe237858600892193434d
Abdul <abdulkaleem8360@gmail.com>
2024-03-09 20:37:58 +0530

    Added 2244th leetcode problem

---
+++ MinimumRoundsToCompleteTask(2244).java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+class Solution {
+    public int minimumRounds(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        int count = 0;
+        for (HashMap.Entry<Integer, Integer> m : map.entrySet()) {
+            int k = m.getValue();
+            if (k == 1) {
+                return -1;
+            } 
+            count=count+k/3;
+            if(k%3!=0){
+                count++;
+            }
+
+        }
+        return count;
+    }
+}
