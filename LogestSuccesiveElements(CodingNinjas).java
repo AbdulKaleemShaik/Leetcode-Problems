b0dbdacb420cc8be669052d6c75024c08c46d184
Abdul <abdulkaleem8360@gmail.com>
2024-04-10 17:57:52 +0530

    Added longestsuccessiveElements problem

---
+++ LogestSuccesiveElements(CodingNinjas).java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+ class Solution {
+    public static int longestSuccessiveElements(int []a) {
+       int n = a.length;
+        if (n == 0)
+            return 0;
+        int longest = 1;
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            set.add(a[i]);
+        }
+        for (int it : set) {
+            if (!set.contains(it - 1)) {
+                int cnt = 1;
+                int x = it;
+                while (set.contains(x + 1)) {
+                    x = x + 1;
+                    cnt = cnt + 1;
+                }
+                longest = Math.max(longest, cnt);
+            }
+        }
+        return longest;
+    }
+}
