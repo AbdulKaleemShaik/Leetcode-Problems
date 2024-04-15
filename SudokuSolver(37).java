159b19f56695db46ac3deba197930873e7fbb8c8
Abdul <abdulkaleem8360@gmail.com>
2024-04-15 13:12:17 +0530

    Added 37th leetcode problem

---
+++ SudokuSolver(37).java
@@ -0,0 +1,40 @@
+
+class Solution {
+    public static boolean solveSudoku(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == '.') {
+                    for (char ch = '1'; ch <= '9'; ch++) {
+
+                        if (find(board, i, j, ch)) {
+                            board[i][j] = ch;
+                            if (solveSudoku(board)) {
+                                return true;
+                            } else {
+                                board[i][j] = '.';
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean find(char[][] board, int row, int col, char ch) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == ch) {
+                return false;
+            }
+            if (board[i][col] == ch) {
+                return false;
+            }
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == ch) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
