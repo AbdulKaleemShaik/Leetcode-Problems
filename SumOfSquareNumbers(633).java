e5a134f90ec5441b41fad7ed9cfc054630a48269
Abdul <abdulkaleem8360@gmail.com>
2024-06-17 14:59:36 +0530

    Added 633rd leetcode problem

---
+++ SumOfSquareNumbers(633).java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        long i = 0;
+        long j = (long) Math.sqrt(c);
+        while (i <= j) {
+            long sum = i * i + j * j;
+            if (sum < c)
+                i++;
+            else if (sum > c)
+                j--;
+            else
+                return true;
+        }
+        return false;
+    }
+}
