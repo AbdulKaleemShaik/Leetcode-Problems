693e81ca03b7b31ff487d0d30c25b455ada72f27
Abdul <abdulkaleem8360@gmail.com>
2024-07-18 11:26:51 +0530

    Added 1530th leetcode problem

---
+++ NumberOfGoodPairsLeafNodes.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int cnt = 0;
+ 
+     public int countPairs(TreeNode root, int distance) {
+         dfs(root, distance);
+         return cnt;
+     }
+ 
+     private List<Integer> dfs(TreeNode root, int distance) {
+         if (root == null) {
+             return new ArrayList<>();
+         }
+         List<Integer> lefIntegers = dfs(root.left, distance);
+         List<Integer> righIntegers = dfs(root.right, distance);
+         if (lefIntegers.size() == 0 && righIntegers.size() == 0) {
+             List<Integer> temp = new ArrayList<>();
+             temp.add(1);
+             return temp;
+         }
+         for (Integer i : lefIntegers) {
+             for (Integer j : righIntegers) {
+                 if ( i + j <= distance)
+                     cnt++;
+             }
+         }
+         List<Integer> temp = new ArrayList<>();
+         for (int i : lefIntegers) {
+                 temp.add(i + 1);
+         }
+         for (int i : righIntegers) {
+                 temp.add(i + 1);
+         }
+         return temp;
+     }
+ }
