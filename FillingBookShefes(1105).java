3c26093cbe9c97d84e54cfd49bb6ff0221ea763d
Abdul <abdulkaleem8360@gmail.com>
2024-07-31 22:10:30 +0530

    Added 1105th leetcode problem

---
+++ FillingBookShefes(1105).java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+class Solution {
+    public int minHeightShelves(int[][] books, int shelfWidth) {
+        int n = books.length;
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int width = 0;
+            int height = 0;
+            for (int j = i; j > 0; j--) {
+                width += books[j - 1][0];
+                if (width > shelfWidth) break;
+
+                height = Math.max(height, books[j - 1][1]);
+                dp[i] = Math.min(dp[i], dp[j - 1] + height);
+            }
+        }
+
+        return dp[n];
+    }
+}
