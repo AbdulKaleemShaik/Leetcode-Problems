2bbc47a12904ee756a12fb9806925a8cbcff371c
Abdul <abdulkaleem8360@gmail.com>
2024-08-04 23:36:21 +0530

    Added 1508th leetcode problem

--- MakeTwoArraysEqualByReversingSubArrays(1460).java
+++ MakeTwoArraysEqualByReversingSubArrays(1460).java
@@ -1,20 +1,12 @@
 class Solution {
     public boolean canBeEqual(int[] target, int[] arr) {
-        //  Arrays.sort(target);
+        // Arrays.sort(target);
         // Arrays.sort(arr);
         // for (int i = 0; i < arr.length; i++) {
-        //     if (arr[i] != target[i])
-        //         return false;
+        // if (arr[i] != target[i])
+        // return false;
         // }
         // return true;
-         int[] count = new int[1001];
-        for(int i=0;i<target.length;i++){
-            count[target[i]]++;
-            count[arr[i]]--;
-        }
-        for(int i=0;i<count.length;i++){
-            if(count[i]!=0)return false;
-        }
-        return true;
+         
     }
 }
