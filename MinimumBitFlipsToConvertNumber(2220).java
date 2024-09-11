eacc537ff4fbf99f7f5f0ecafbb245577dbc3bf4
Abdul Kaleem Shaik <abdulkaleem8360@gmail.com>
2024-09-11 10:34:47 +0530

    Solved 2220th leetcode problem

---
+++ MinimumBitFlipsToConvertNumber(2220).java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+         int ans = 0;
+        while (start != 0 || goal != 0) {
+            if ((start & 1) != (goal & 1))
+                ans++;
+            start >>= 1;
+            goal >>= 1;
+
+        }
+        return ans;
+    }
+}
